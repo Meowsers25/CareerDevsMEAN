3389bef735403cddcb7dc91664df7b037aef1ae6
Chris <c.lamothe25@gmail.com>
2017-09-20 15:52:28 -0400

    added level 7

---
+++ section1-level7/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+
+//.listen is asynchronous
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log('Magic happens on Port: ' + port);
+});
+
+//this prints first to prove that .listen is asynchronous
+//console.log('Me first!!');
